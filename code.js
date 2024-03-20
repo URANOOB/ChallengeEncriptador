28352775d1ccb8c2608f250029e71ba7dafb5ad3
William Galeano <will21@hotmail.es>
2024-03-19 22:18:16 -0500

    Adding new features

--- code.js
+++ code.js
@@ -1,9 +1,27 @@
-function desencriptarTexto(textoEncriptado) {
-    // Definir las reglas de desencriptación
-    const reglasDesencriptacion = {
-        'enter': 'e',
-        'imes': 'i',
-        'ai': 'a',
-        'ober': 'o',
-        'ufat': 'u'
-    };
+function encriptarTexto(text) {
+    
+    // Input del texto a ser encriptado y conversion para que siempre sea en minusculas
+    const promptText = prompt('Texto a ser encriptado');
+    text = promptText.toLowerCase();
+
+    // Reglas de desencriptación
+    const reglasEncriptacion = {
+        'e': 'enter',
+        'i': 'imes',
+        'a': 'ai',
+        'o': 'ober',
+        'u': 'ufat'
+    }
+    
+    for (let id in reglasEncriptacion)  {
+        const regla = new RegExp(id, 'gi');
+        text = text.replace(regla, reglasEncriptacion[id]);
+    }
+
+    return console.log(text);
+};
+
+
+
+
+encriptarTexto();
